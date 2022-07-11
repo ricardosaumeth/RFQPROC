ada2219a43d56c10148ad53e269c38e4156103b4
“Ricardo <ricardo.saumeth@playtech.com>
2022-07-11 13:51:34 +0100

    Add comment

--- backend/src/rabbitmq/sender.ts
+++ backend/src/rabbitmq/sender.ts
@@ -29,6 +29,7 @@ amqp.connect('amqp://localhost', (connError: string, connection: Connection) => 
           channel.sendToQueue(queue, Buffer.from(JSON.stringify(line)));
           startIndex = ch + 1;
           csvTotalLines.push(line);
+          // uncomment the log to see the lines. It will slow the proces
           // console.log(`Sending line: ${line}`)
         }
       }
