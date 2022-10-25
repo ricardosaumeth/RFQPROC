2a62f0a60409bd9afa3c2ac9acf0272c8c622877
“Ricardo <ricardo.saumeth@playtech.com>
2022-10-25 11:49:56 +0100

    add right font family

--- packages/client/src/modules/book/components/DepthChart/DepthChart.styled.ts
+++ packages/client/src/modules/book/components/DepthChart/DepthChart.styled.ts
@@ -3,5 +3,9 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   height: 100%;
+  
+  .highcharts-title {
+    font-family: FiraSans-Regular;
+  }
 `;
 
